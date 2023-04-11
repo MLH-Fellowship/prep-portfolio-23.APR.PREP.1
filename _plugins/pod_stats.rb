da01afcabec37645befd749c5072d910e1957536
quanrong <quanrong@mailbox.org>
2023-04-11 10:34:40 +0200

    Update github username key in fellow hash

--- _plugins/pod_stats.rb
+++ _plugins/pod_stats.rb
@@ -13,8 +13,6 @@ module PodStats
       # list of MLH repos the pod is working on
       projects = site.data['projects']
       fellows = site.data['fellows'] # list of pod fellows
-      # TODO: update 'name' with the actual key (e.g. 'github')
-      # as soon as issue 13 gets solved
       fellows.each { |f| f['merged'] = f['commits'] = 0 }
       projects.map { |p| p['repo'] }.each do |repo|
         repo['commits'] = repo['merged'] = 0
@@ -29,23 +27,23 @@ module PodStats
 
 
     def add_commits!(fellows, repo)
-      usernames = fellows.map { |f| f['name'] }
+      usernames = fellows.map { |f| f['github'] }
       uri = URI("https://api.github.com/repos/#{repo['owner']}/#{repo['name']}/stats/contributors")
       contributors = get_as_json(uri)
       contributors.each do |contr|
         username = contr['author']['login']
         next unless usernames.include?(username)
 
-        commits = contr['total']
+        commits = contr['total'].to_i
         repo['commits'] += commits
-        fellow = fellows.select { |f| f['name'] == username }
+        fellow = fellows.detect { |f| f['github'] == username }
         fellow['commits'] += commits
       end
     end
 
 
     def add_merges!(fellows, repo)
-      usernames = fellows.map { |f| f['name'] }
+      usernames = fellows.map { |f| f['github'] }
       uri = URI("https://api.github.com/search/issues?q=repo:#{repo['owner']}/#{repo['name']}+is:pr+is:merged")
       merges = get_as_json(uri)
       merges['items'].each do |merge|
@@ -53,7 +51,7 @@ module PodStats
         next unless usernames.include?(username)
 
         repo['merged'] += 1
-        fellows.select { |f| f['name'] == username }
+        fellow = fellows.detect { |f| f['github'] == username }
         fellow['merged'] += 1
       end
     end
